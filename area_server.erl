a13a6f522418181a4da67b090c793eae1ed54bd6
Joaquín Benavides <joaquin.benavides@sumup.com>
2021-03-02 09:15:25 -0300

    first server erl

---
+++ area_server.erl
@@ -0,0 +1,32 @@
+-module(area_server).
+
+-export([start/0, loop/0, area/2]).
+
+start() -> spawn(area_server, loop, []).
+
+area(Pid, What) ->
+    rpc(Pid, What).
+
+rpc(Pid, Request) ->
+    Pid ! {self(), Request},
+    receive
+        {Pid, Response} ->
+            Response
+    end.
+
+loop() ->
+    receive
+        {From, {rectangle, Width, Ht}} ->
+            From ! {self(), Width * Ht},
+            loop();
+        {From, {circle, R}} ->
+            From ! {self(), 3.14259 * R * R},
+            loop();
+        {From, {square, Side}} ->
+            From ! {self(), Side * Side},
+            loop();
+        {From, Other} ->
+            From ! {self(), error, Other},
+            loop()
+        end.
+
